236c4ff3a621ce539489291fff7136ce548de9ed
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2021-02-08 12:35:46 -0500

    link extractor

---
+++ utils/get_links.py
@@ -0,0 +1,44 @@
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# set up driver options
+options = Options()
+options.add_argument("--headless")
+
+
+def scroll(driver, timeout):
+    flag = 1
+
+    # Get current scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while flag == 1:
+
+        # Scroll to bottom and wait to load
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(timeout)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            print("end of page reached")
+            flag = 0
+
+        last_height = new_height
+
+
+def get_source(url):
+    # set up driver
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+
+    scroll(driver, 3)
+
+    source = driver.page_source
+
+    driver.close()
+
+    return source
