b4c0f214696cc59855aa9d2ebd361984b520f7e4
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2021-02-09 05:30:32 -0500

    organized repo

--- utils/get_links.py
+++ utils/get_links.py
@@ -1,5 +1,4 @@
 import time
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -18,6 +17,7 @@ def scroll(driver, timeout):
 
         # Scroll to bottom and wait to load
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        print("scrolling...")
         time.sleep(timeout)
 
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -29,13 +29,13 @@ def scroll(driver, timeout):
         last_height = new_height
 
 
-def get_source(url):
+def get_source(url, timeout=5):
     # set up driver
     driver = webdriver.Chrome(options=options)
 
     driver.get(url)
 
-    scroll(driver, 3)
+    scroll(driver, timeout)
 
     source = driver.page_source
 
